a1f154376d542a1986c85fc3f15f6577ef41877c
Andre Will <andrewill07@hotmail.com>
2023-03-06 20:26:55 -0300

    Ajuste

--- ProgramWeb2/Classes/body_classe.php
+++ ProgramWeb2/Classes/body_classe.php
@@ -4,12 +4,15 @@ class body_classe{
 
     public function bodyHtml(){
         echo '<body>';
-        echo  '<div>';
-        echo    "<menu>  
-                  <li><a href=''>TESTE</a></li>  
-                  <li><a href=''>TESTE</a></li>  
-                  <li><a href=''>TESTE</a></li>  
-                  <li><a href=''>TESTE</a></li>  
+        echo  '<h1> Atividade de Quarta </h1>';
+        echo  '<div style="background-color: lightblue; width: 300px;">';
+        echo    "<menu>
+                    <ol>    
+                        <li><a href=''>TESTE</a></li>  
+                        <li><a href=''>TESTE</a></li>  
+                        <li><a href=''>TESTE</a></li>  
+                        <li><a href=''>TESTE</a></li>  
+                    </ol>
                 </menu>  ";
         echo '</div>';
         echo '</body>';
